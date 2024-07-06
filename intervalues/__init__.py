711717d293e183407f147921e5d878d254bfd223
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-07-06 15:52:39 -0400

    Use ValueCounter for relevant tests in test_combine_intervals

--- intervalues/__init__.py
+++ intervalues/__init__.py
@@ -1,4 +1,4 @@
 from .interval_counter import IntervalCounterFloat
-from .base_interval import BaseInterval
+from .base_interval import BaseInterval, ValueInterval
 from .combine_intervals import combine_intervals
 from .abstract_interval import AbstractInterval
