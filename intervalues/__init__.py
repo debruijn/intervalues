ff1c583495d2eea6f0021c09d8b74555378d350b
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-07-11 14:41:19 -0400

    Fixes to published version

--- intervalues/__init__.py
+++ intervalues/__init__.py
@@ -4,7 +4,4 @@ from .interval_set import IntervalSetFloat
 from .base_interval import BaseInterval, UnitInterval, EmptyInterval
 from .base_interval import ValueInterval as _ValueInterval
 from .combine_intervals import combine_intervals
-
-
-with open('VERSION') as version_file:
-    __version__ = version_file.read().strip()
+from .__version__ import __version__
