de86cbf7b4767ef757a99a8bee243369c2bba060
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-07-19 16:49:47 -0400

    Small fix to existing example

--- examples/example_count_criteria_met.py
+++ examples/example_count_criteria_met.py
@@ -1,19 +1,19 @@
 # This example shows an example of how to use this, in this case for finding a value in a range 0 to 100 that meets the
-# most requirements. The requirements could be for example functions that need to exceed 0 to be met, and as a starting
-# point here I assume it has already been detected for which values these functions are positive.
+# most requirements out of a total of 250. The requirements could be for example functions that need to exceed 0 to be
+# met, and as a starting point here I assume it has already been detected for which values these functions are positive.
 
 from random import random
-from intervalues import IntervalCounterFloat, BaseInterval
+import intervalues as iv
 
 n_reqs = 250
-reqs = [(random()*100, random()*100) for _ in range(n_reqs)]
+reqs = [(random()*100, random()*100) for _ in range(n_reqs)]  # For which interval each requirement is positive
 
-# Convert reqs to intervals
-intervals = [BaseInterval((min(x), max(x))) for x in reqs]
+# Convert requirements to intervals
+intervals = [iv.BaseInterval((min(x), max(x))) for x in reqs]
 
 # Combine intervals to one Counter
-interval_counter = IntervalCounterFloat(intervals.copy())
-interval_counter_one_by_one = IntervalCounterFloat()
+interval_counter = iv.IntervalCounter(intervals.copy())
+interval_counter_one_by_one = iv.IntervalCounter()
 for interval in intervals:
     interval_counter_one_by_one.update(interval)
 
@@ -22,4 +22,4 @@ print(f"Counter total: {interval_counter.total_length():.4f}; "
       f"Counter one by one: {interval_counter_one_by_one.total_length():.4f}; "
       f"Sum of individual intervals: {sum(x.get_length() for x in intervals):.4f}")
 print(f"Number of final subintervals: {len(interval_counter.data)}")
-print(f"The most common (or one of): {interval_counter.most_common(1)}")
+print(f"The most common subinterval: {interval_counter.most_common(1)}")
